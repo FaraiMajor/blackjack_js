9c4c3febd4c0e13eea76669562bdaeb51a704b9b
Farai Major <faraimutukumira@gmail.com>
2023-02-22 01:54:45 -0500

    blackjack

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Blackjack with JavaScript
+
+<!-- 
+![pong game](pong.gif) -->
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+    name: "Per",
+    chips: 200
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,6 +11,10 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
